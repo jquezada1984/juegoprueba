79616669b09cb9e861ada1add0e0485c17f93ee7
jquezada1984 <johnquezadahuayamave@gmail.com>
2024-05-09 21:33:15 -0500

    presentando el gato
    presentando el gato.

--- paginas/Nivel1.js
+++ paginas/Nivel1.js
@@ -7,9 +7,20 @@ class Nivel1 extends Phaser.Scene{
     }
 
     preload(){
-        this.load.image("fondo",".//resources/fondo.jpg");   
+        /******************Fondos**************************************/
+        this.load.image("fondo",".//resources/fondo.jpg");
+        this.load.image("suelo",".//resources/plataformacarbon.png");
+        
+        /*********************Player********************************************/
+        this.load.image("player",".//resources/playergato.png");   
     }
     create(){
         this.add.image(250,250,'fondo');
+        this.suelos=this.physics.add.staticGroup();
+        this.suelos.create(250,480,'suelo').setScale(0.5).refreshBody();
+        this.gato=this.physics.add.sprite(250,300,'player');
+        this.gato.setBounce(0.2);
+        this.gato.setCollideWorldBounds(true);
+        this.physics.add.collider(this.gato,this.suelos);
     }
 }
