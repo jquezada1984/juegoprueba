f3bc8172dfbbc8808c32c50a0fda0a585478b85b
jquezada1984 <johnquezadahuayamave@gmail.com>
2024-05-12 23:44:28 -0500

    Escalado del mundo
    trabajando en el primer Nivel.

--- paginas/Nivel1.js
+++ paginas/Nivel1.js
@@ -1,6 +1,7 @@
 class Nivel1 extends Phaser.Scene{
-    suelos;
+    pisos;
     gato;
+    canJump;
 
     constructor () {
         super ("Nivel1"); // nombre escena
@@ -9,29 +10,54 @@ class Nivel1 extends Phaser.Scene{
     preload(){
         /******************Fondos**************************************/
         this.load.image("fondo",".//resources/fondo.jpg");
-        this.load.image("suelo",".//resources/plataformacarbon.png");
-        
         /*********************Player********************************************/
         this.load.image("player",".//resources/playergato.png");   
+        //////////////////Pisos///////////
+        this.load.image("Pisos",".//resources/Pisos.png"); // donde estan los patrones
+        this.load.tilemapTiledJSON("tilemap",".//resources/PisoJuego.json") // donde esta el achivo json
     }
     create(){
-        this.add.image(250,250,'fondo');
-        this.suelos=this.physics.add.staticGroup();
-        this.suelos.create(250,480,'suelo').setScale(0.5).refreshBody();
+        const fondo=this.add.image(250,250,'fondo');
+        fondo.setDepth(-1);
+
+
+        const map = this.make.tilemap({key:"tilemap"});
+        const tileset = map.addTilesetImage("picho","Pisos");
+        
+        this.pisos = map.createLayer("PisosJohnKleber",tileset);
+        this.pisos.setCollisionByProperty({colicion:true});
+
+////////////////////////////////////////////////////////
         this.gato=this.physics.add.sprite(250,300,'player');
         this.gato.setBounce(0.2);
         this.gato.setCollideWorldBounds(true);
-        this.physics.add.collider(this.gato,this.suelos);
+
         this.cursor=this.input.keyboard.createCursorKeys();
+        this.physics.add.collider(this.gato,this.pisos)
+
+        // Inicializa el indicador de salto
+        this.canJump = true;
     }
 
     update(){
         if(this.cursor.left.isDown){
-            console.log('hey');
             this.gato.setVelocityX(-160);
+        }
+        else if(this.cursor.right.isDown){
+            this.gato.setVelocityX(+160);
         }
         else{
             this.gato.setVelocityX(0);
         }
+        
+    if (this.cursor.up.isDown && this.canJump) {
+        this.gato.setVelocityY(-250);
+        this.canJump = false;
+    }
+
+
+    if (this.cursor.up.isUp && !this.gato.body.touching.down) {
+        this.canJump = true;
+    }
     }
 }
